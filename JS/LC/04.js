cfb5c47f0ff269b9f47b6d62029b66b2b3348db5
Shahriar Ahmed ~ SHIFAT <shahriar.ahmed.7878@gmail.com>
2025-11-20 11:58:47 +0600

    Add examples for converting values to BOOLEAN in JavaScript

--- JS/LC/04.js
+++ JS/LC/04.js
@@ -27,3 +27,24 @@ parseFloat("3.14em"); //? 3.14 (it stops at 'e')
 parseInt("Chapter 2"); //? NaN (because it doesn't start with a number)
 
 Number("100px"); //? NaN (because the whole string is not a valid number)
+
+/**
+ @TODO The Unary Plus + Operator (A common trick)
+ @TODO Placing a + in front of a value is a concise way to convert it to a number, following the same rules as the Number() function.
+ */
+let str1 = "50";
+let num2 = +str1; //? 50 (as a number)
+
+//* Converting to "BOOLEAN"
+Boolean(0); // false
+Boolean(""); // false
+Boolean(null); // false
+Boolean(undefined); // false
+Boolean(NaN); // false
+
+// Everything else is truthy!
+Boolean(100); // true
+Boolean("hello"); // true
+Boolean("false"); // true (a non-empty string is truthy)
+Boolean([]); // true (an empty array is an object, and is truthy)
+Boolean({}); // true (an empty object is truthy)
