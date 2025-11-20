a668590f38a1469fb1814a6991d08c3d35594243
Shahriar Ahmed ~ SHIFAT <shahriar.ahmed.7878@gmail.com>
2025-11-20 12:00:37 +0600

    Refactor comments for clarity and consistency in BOOLEAN conversion examples

--- JS/LC/04.js
+++ JS/LC/04.js
@@ -29,22 +29,21 @@ parseInt("Chapter 2"); //? NaN (because it doesn't start with a number)
 Number("100px"); //? NaN (because the whole string is not a valid number)
 
 /**
- @TODO The Unary Plus + Operator (A common trick)
- @TODO Placing a + in front of a value is a concise way to convert it to a number, following the same rules as the Number() function.
+ //TODO The Unary Plus + Operator (A common trick)
+ //TODO Placing a + in front of a value is a concise way to convert it to a number, following the same rules as the Number() function.
  */
 let str1 = "50";
 let num2 = +str1; //? 50 (as a number)
 
 //* Converting to "BOOLEAN"
-Boolean(0); // false
-Boolean(""); // false
-Boolean(null); // false
-Boolean(undefined); // false
-Boolean(NaN); // false
-
-// Everything else is truthy!
-Boolean(100); // true
-Boolean("hello"); // true
-Boolean("false"); // true (a non-empty string is truthy)
-Boolean([]); // true (an empty array is an object, and is truthy)
-Boolean({}); // true (an empty object is truthy)
+Boolean(0); //? false
+Boolean(""); //? false
+Boolean(null); //? false
+Boolean(undefined); //? false
+Boolean(NaN); //? false
+//? Everything else is truthy!
+Boolean(100); //? true
+Boolean("hello"); //? true
+Boolean("false"); //? true (a non-empty string is truthy)
+Boolean([]); //? true (an empty array is an object, and is truthy)
+Boolean({}); //? true (an empty object is truthy)
